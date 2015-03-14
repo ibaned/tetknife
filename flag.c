529202677f711ca3923477452b6c8ee27307d002
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-03-14 10:17:24 -0500

    added gflag system to flag.c

--- flag.c
+++ flag.c
@@ -36,8 +36,49 @@ int flag_get(flag* f, int i)
   return (f->bytes[byte] & (1 << bit)) != 0;
 }
 
-void flag_free(flag* f)
+void flag_dtor(flag* f)
 {
   my_free(f->bytes);
 }
 
+struct gflag {
+  flag f[CAD_ENT_TYPES];
+};
+
+gflag* gflag_new(void)
+{
+  gflag* f;
+  gent_type t;
+  f = my_malloc(sizeof(*f));
+  for (t = 0; t < CAD_ENT_TYPES; ++t)
+    flag_init(f->f + t);
+  return f;
+}
+
+void gflag_grow(gflag* f, gent_type t, unsigned c)
+{
+  flag_grow(f->f + t, c);
+}
+
+void gflag_clear(gflag* f, gent e)
+{
+  flag_clear(f->f + e.t, e.i);
+}
+
+void gflag_set(gflag* f, gent e)
+{
+  flag_set(f->f + e.t, e.i);
+}
+
+int gflag_get(gflag* f, gent e)
+{
+  return flag_get(f->f + e.t, e.i);
+}
+
+void gflag_free(gflag* f)
+{
+  gent_type t;
+  for (t = 0; t < CAD_ENT_TYPES; ++t)
+    flag_dtor(f->f + t);
+  my_free(f);
+}
--- flag.h
+++ flag.h
@@ -1,6 +1,8 @@
 #ifndef FLAG_H
 #define FLAG_H
 
+#include "cad.h"
+
 typedef struct {
   unsigned char* bytes;
 } flag;
@@ -10,6 +12,15 @@ void flag_grow(flag* f, unsigned c);
 void flag_clear(flag* f, int i);
 void flag_set(flag* f, int i);
 int flag_get(flag* f, int i);
-void flag_free(flag* f);
+void flag_dtor(flag* f);
+
+typedef struct gflag gflag;
+
+gflag* gflag_new(void);
+void gflag_grow(gflag* f, gent_type t, unsigned c);
+void gflag_clear(gflag* f, gent e);
+void gflag_set(gflag* f, gent e);
+int gflag_get(gflag* f, gent e);
+void gflag_free(gflag* f);
 
 #endif
