07c3fca30c73a6302e6518e4090195b5138f9bed
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-03-17 08:29:26 -0600

    testing cad_gen_polygon

--- exe/cad_test.c
+++ exe/cad_test.c
@@ -1,12 +1,19 @@
 #include "../cad.h"
+#include "../cad_gen.h"
+#include "../cad_geom.h"
 
 int main()
 {
+  point const pts[4] = {
+    {0,0,0},
+    {1,0,0},
+    {1,1,0},
+    {0,1,0}
+  };
   cad* c;
-  gent e;
   c = cad_new();
-  e = gent_new(c, CAD_VERTEX);
-  gent_free(c, e);
+  geom_new(c);
+  cad_gen_polygon(c, 4, pts);
   cad_free(c);
   return 0;
 }
