d1f9c737ed99835a088d1698ffddb0469fe8336f
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-03-19 12:33:35 -0400

    use C89 on Mac also

--- mac.mk
+++ mac.mk
@@ -1,6 +1,6 @@
 #options are "cocoa", "w32", and "gtk"
 CC = clang
-CFLAGS = -O2 -g -Weverything -Werror
+CFLAGS = -std=c89 -O2 -g -Weverything -Wpedantic -Werror
 GUI = cocoa
 BACK = direct
 TARGETS = test cad_test quality
