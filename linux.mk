64e92aee987b49dc0ea182e43defe0163889c370
Dan Ibanez <ibaned@rpi.edu>
2015-03-19 12:36:41 -0400

    linux errors are warnings, except clang pragmas

--- linux.mk
+++ linux.mk
@@ -1,6 +1,6 @@
 #options are "cocoa", "w32", and "gtk"
 CC = gcc
-CFLAGS = -std=c89 -O2 -g -Wall -Wpedantic
+CFLAGS = -std=c89 -O2 -g -Wall -Wpedantic -Werror -Wno-unknown-pragmas
 LDLIBS = -lm
 GUI = gtk
 BACK = direct
