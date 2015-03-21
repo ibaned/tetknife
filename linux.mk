56d1387c41f6c82adbfb6cf79a77bec59e858204
Dan Ibanez <ibaned@rpi.edu>
2015-03-21 14:18:58 -0400

    updating linux makefile example

--- linux.mk
+++ linux.mk
@@ -1,7 +1,8 @@
 #options are "cocoa", "w32", and "gtk"
 CC = gcc
-CFLAGS = -std=c89 -O2 -g -Wall -Wpedantic -Werror -Wno-unknown-pragmas
+CFLAGS = -std=c89 -O2 -g -Wall -Wpedantic -Wno-unknown-pragmas -Werror -fno-omit-frame-pointer
+LDFLAGS =
 LDLIBS = -lm
 GUI = gtk
 BACK = direct
-TARGETS = test cad_test quality
+TARGETS = test cad_test quality myperf
