172da2af7fe926a183388dcb58b2fe194d84b557
Dan Ibanez <ibaned@rpi.edu>
2015-03-21 13:08:35 -0400

    get prof/speed.sh to clean up after itself

--- prof/speed.sh
+++ prof/speed.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
 perf record -g -- ./$1
 perf script | ./prof/stackcollapse-perf.pl > out.perf-folded
+rm perf.data
 ./prof/flamegraph.pl --title=$1 out.perf-folded > $1.svg
+rm out.perf-folded
