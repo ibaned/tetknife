831c6c55598aa63fc36de2b318bf9a30d400b6a3
Dan Ibanez <ibaned@rpi.edu>
2015-03-21 12:15:06 -0400

    more fixes to uninit fu values

--- mesh.c
+++ mesh.c
@@ -127,13 +127,17 @@ ment ment_new(mesh* m, simplex t, ment const v[])
   ment e;
   muse u;
   unsigned i;
+  simplex ut;
   while (t > m->et)
     raise_dim(m);
   e.t = t;
   if (flex_full(&m->f[t]))
     mesh_grow(m, t);
   e.i = flex_add(&m->f[t]);
-  if (t != VERTEX)
+  if (t == VERTEX)
+    for (ut = EDGE; ut <= m->et; ++ut)
+      m->fu[ut][e.i] = NULL_IDX;
+  else
     for (i = 0; i < nverts(t); ++i) {
       u = muse_at(e, i);
       ASSERT(v[i].t == VERTEX);
