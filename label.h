63063cbf6752ebe4c2c104556d40b84ac2e47bf3
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-04-14 08:40:21 -0400

    attribute pure for mlabel_get

--- label.h
+++ label.h
@@ -8,7 +8,7 @@ typedef struct mlabel mlabel;
 mlabel* mlabel_new(mesh* m);
 void mlabel_free(mlabel* l);
 
-long mlabel_get(mlabel* l, ment e);
+long mlabel_get(mlabel* l, ment e) __attribute__((pure));
 void mlabel_set(mlabel* l, ment e, long v);
 
 #endif
