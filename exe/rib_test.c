c566674a6a95a620f4557a66f112f790e571a4b9
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-04-16 09:21:50 -0400

    latest rib_test works

--- exe/rib_test.c
+++ exe/rib_test.c
@@ -7,27 +7,38 @@ int main()
   unsigned n;
   point* o;
   rcopy* rc;
-  unsigned i;
+  int i;
+  unsigned j;
   mpi_init();
   comm_init(mpi_world());
-  o = my_malloc(sizeof(point) * 2);
-  rc = my_malloc(sizeof(rcopy) * 2);
+  n = 3;
+  o = my_malloc(sizeof(point) * n);
+  rc = my_malloc(sizeof(rcopy) * n);
   if (comm_rank() == 0) {
     o[0] = point_new(0,0,0);
     o[1] = point_new(1,1,0);
+    o[2] = point_new(.3,.5,0);
   } else {
     o[0] = point_new(1,0,0);
     o[1] = point_new(0,1,0);
+    o[2] = point_new(.5,.3,0);
   }
-  rc[0].rank = comm_rank();
-  rc[1].rank = comm_rank();
-  rc[0].ri = 0;
-  rc[1].ri = 0;
+  for (j = 0; j < n; ++j) {
+    rc[j].rank = comm_rank();
+    rc[j].ri = (int) j;
+  }
   inertial_bisection(&n, &o, &rc);
-  print("post n = %u\n", n);
-  for (i = 0; i < n; ++i) {
-    print(" (%f,%f,%f)", o[i].x, o[i].y, o[i].z);
-    print(" (%d,%d)", rc[i].rank, rc[i].ri);
+  for (i = 0; i < comm_size(); ++i) {
+    if (comm_rank() == i) {
+      print("rank %d\n", i);
+      print("post n = %u\n", n);
+      for (j = 0; j < n; ++j) {
+        print(" (%f,%f,%f)", o[j].x, o[j].y, o[j].z);
+        print(" (%d,%d)", rc[j].rank, rc[j].ri);
+        print("\n");
+      }
+    }
+    mpi_barrier(comm_mpi());
   }
   comm_finalize();
   mpi_finalize();
