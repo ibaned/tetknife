17e5a31bfda93ce860818f293ba19075ce3c4dbc
Dan Ibanez <ibaned@rpi.edu>
2015-04-16 11:20:52 -0400

    4-proc RIB test works !

--- exe/rib_test.c
+++ exe/rib_test.c
@@ -9,25 +9,40 @@ int main()
   rcopy* rc;
   int i;
   unsigned j;
+  point allpts[8] = {
+    { 1, 2,0},
+    { 2, 1,0},
+    { 1,-2,0},
+    { 2,-1,0},
+    {-1, 2,0},
+    {-2, 1,0},
+    {-1,-2,0},
+    {-2,-1,0}
+  };
   mpi_init();
   comm_init(mpi_world());
-  n = 3;
+  n = 2;
   o = my_malloc(sizeof(point) * n);
   rc = my_malloc(sizeof(rcopy) * n);
+  ASSERT(comm_size() == 4);
   if (comm_rank() == 0) {
-    o[0] = point_new(0,0,0);
-    o[1] = point_new(1,1,0);
-    o[2] = point_new(.3,.5,0);
-  } else {
-    o[0] = point_new(1,0,0);
-    o[1] = point_new(0,1,0);
-    o[2] = point_new(.5,.3,0);
+    o[0] = allpts[3];
+    o[1] = allpts[5];
+  } else if (comm_rank() == 1) {
+    o[0] = allpts[1];
+    o[1] = allpts[4];
+  } else if (comm_rank() == 2) {
+    o[0] = allpts[6];
+    o[1] = allpts[0];
+  } else if (comm_rank() == 3) {
+    o[0] = allpts[2];
+    o[1] = allpts[7];
   }
   for (j = 0; j < n; ++j) {
     rc[j].rank = comm_rank();
     rc[j].ri = (int) j;
   }
-  inertial_bisection(&n, &o, &rc);
+  recursive_inertial_bisection(&n, &o, &rc);
   for (i = 0; i < comm_size(); ++i) {
     if (comm_rank() == i) {
       print("rank %d\n", i);
