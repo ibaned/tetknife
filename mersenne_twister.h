660657e9a8f3a9205f10827d296d3066c6b73495
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-04-19 19:17:29 -0400

    mersenne_twister_color

--- mersenne_twister.h
+++ mersenne_twister.h
@@ -1,8 +1,11 @@
 #ifndef MERSENNE_TWISTER_H
 #define MERSENNE_TWISTER_H
 
+#include "image.h"
+
 void mersenne_twister_seed(unsigned seed);
 unsigned mersenne_twister(void);
 double mersenne_twister_real(void);
+color mersenne_twister_color(void);
 
 #endif
