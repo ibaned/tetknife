3e48f2cdaa13e085dda8d169c6858317059d02d6
Dan Ibanez <ibaned@rpi.edu>
2015-05-16 11:21:09 -0400

    print BG/Q memory stats

--- exe/scaling.c
+++ exe/scaling.c
@@ -6,6 +6,7 @@
 #include "../remotes.h"
 #include "../rib.h"
 #include "../cavity_op.h"
+#include <spi/include/kernel/memory.h>
 
 static void print_value_stats(char const* name, unsigned ln)
 {
@@ -29,6 +30,17 @@ static void print_mesh_stats(mesh* m)
   print_value_stats("nneigh", rpeer_count(m));
 }
 
+static void print_memory_stats(void)
+{
+  uint64_t heap, heapmax, hmmap;
+  Kernel_GetMemorySize(KERNEL_MEMSIZE_HEAP, &heap);
+  Kernel_GetMemorySize(KERNEL_MEMSIZE_HEAPMAX, &heapmax);
+  Kernel_GetMemorySize(KERNEL_MEMSIZE_MMAP, &hmmap);
+  print_value_stats("heap", heap);
+  print_value_stats("heapmax", heapmax);
+  print_value_stats("mmap", hmmap);
+}
+
 static void refine_subgroup(mesh* m, int ngroups)
 {
   mpi* oldcomm;
@@ -44,6 +56,7 @@ static void refine_subgroup(mesh* m, int ngroups)
     if (!comm_rank())
       print("refine, comm_size %d\n", comm_size());
     print_mesh_stats(m);
+    print_memory_stats();
   }
   mesh_exit_groups(m, oldcomm);
   mpi_barrier(comm_mpi());
@@ -67,6 +80,7 @@ static void balance_subgroup(mesh* m, int ngroups)
     if (!comm_rank())
       print("balance, comm_size %d\n", comm_size());
     print_mesh_stats(m);
+    print_memory_stats();
   }
   mesh_exit_groups(m, oldcomm);
   mpi_barrier(comm_mpi());
@@ -89,14 +103,16 @@ int main()
   t0 = mpi_time();
   comm_init(mpi_world());
   ngroups = comm_size();
+  print_memory_stats();
   m = mesh_new();
   if (!comm_rank())
     mesh_gen_bbox(m, b, DIM3, bv, be);
   else
     mesh_set_elem(m, TET);
   remotes_new(m);
-  for (i = 0; i < initial_refines; ++i)
+  for (i = 0; i < initial_refines; ++i) {
     refine_subgroup(m, ngroups);
+  }
   while (ngroups > 1) {
     ASSERT(ngroups % 2 == 0);
     refine_subgroup(m, ngroups);
