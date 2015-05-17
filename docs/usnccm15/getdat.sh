bc9f6ef697d39eea4c5d7e1334c8fe0557b57c7f
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-05-17 12:16:07 -0400

    more post-processing work

--- docs/usnccm15/getdat.sh
+++ docs/usnccm15/getdat.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 grep "^nelem" best.out > nelem.txt
 grep "^nvert" best.out > nvert.txt
-NLINES=`wc -l nelem.txt | cut -d ' ' -f 7`
+grep "^heap " best.out > heap.txt
 head -n 14 nelem.txt > grow_nelem.txt
 head -n 14 nvert.txt > grow_nvert.txt
-NLINES=`expr $NLINES - 14`
-tail -n $NLINES nelem.txt > scale_nelem.txt
-tail -n $NLINES nvert.txt > scale_nvert.txt
+head -n 15 heap.txt > grow_heap.txt
+tail -n 28 nelem.txt > scale_nelem.txt
+tail -n 28 nvert.txt > scale_nvert.txt
+tail -n 28 heap.txt > scale_heap.txt
 awk 'NR%2 == 1' scale_nelem.txt > scl_ref_nelem.txt
 awk 'NR%2 == 1' scale_nvert.txt > scl_ref_nvert.txt
+awk 'NR%2 == 1' scale_heap.txt > scl_ref_heap.txt
+awk 'NR%2 == 0' scale_heap.txt > scl_bal_heap.txt
 cut -d ' ' -f 7 scl_ref_nelem.txt > scl_ref_nelem_total.txt
 cut -d ' ' -f 11 scl_ref_nelem.txt > scl_ref_nelem_imb.txt
 cut -d ' ' -f 11 scl_ref_nvert.txt > scl_ref_nvert_imb.txt
+cut -d ' ' -f 3 scl_bal_heap.txt > scl_bal_heap_max.txt
+cut -d ' ' -f 9 scl_bal_heap.txt > scl_bal_heap_avg.txt
 grep "^balance, comm_size" best.out | cut -d ' ' -f 3 > scl_bal_cs.txt
 grep "^refine, comm_size" best.out | cut -d ' ' -f 3 | tail -n 14 > scl_ref_cs.txt
 paste scl_ref_cs.txt scl_ref_nelem_imb.txt scl_ref_nvert_imb.txt > scl_ref_imb.dat
+paste scl_bal_cs.txt scl_bal_heap_max.txt scl_bal_heap_avg.txt > scl_bal_heap.dat
 gnuplot < scl_ref_imb.plt
+gnuplot < scl_bal_heap.plt
---
+++ docs/usnccm15/scl_bal_heap.plt
@@ -0,0 +1,8 @@
+set terminal svg size 350,262 fname 'Verdana' fsize 10
+set output 'scl_bal_heap.svg'
+set logscale x 2
+set yrange [0:1e8]
+plot 'scl_bal_heap.dat' using 1:2 title "max heap" with linespoints, \
+     'scl_bal_heap.dat' using 1:3 title "avg heap" with linespoints, \
+     11.6e6 title "initial heap" with line
+
